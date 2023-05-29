6c9b74114672ecdd3930ed0841742a6f7a8b875d
Hanmin Kim <hanminkim123@gmail.com>
2023-05-28 22:10:02 -0700

    feat(thread_pool): add thread pool

---
+++ src/thread_pool/thread_pool.cpp
@@ -0,0 +1,56 @@
+#include "thread_pool.h"
+
+void ThreadPool::thread_loop() {
+  while (true) {
+    std::function<void()> job;
+    {
+      std::unique_lock<std::mutex> lock(queue_mutex);
+      mutex_condition.wait(lock, [this] {
+        return !jobs.empty() || should_terminate;
+      });
+      if (should_terminate) {
+        return;
+      }
+      job = jobs.front();
+      jobs.pop();
+    }
+    job();
+  }
+}
+
+void ThreadPool::queue_job(const std::function<void()>& job) {
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    jobs.push(job);
+  }
+  mutex_condition.notify_one();
+}
+
+void ThreadPool::stop() {
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    should_terminate = true;
+  }
+  mutex_condition.notify_all();
+  for (std::thread& active_thread : threads) {
+    active_thread.join();
+  }
+  threads.clear();
+}
+
+bool ThreadPool::busy() {
+  bool pool_busy;
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    pool_busy = jobs.empty();
+  }
+  return pool_busy;
+}
+
+ThreadPool::ThreadPool(int max_threads) {
+  max_t = std::min((int) std::thread::hardware_concurrency(), max_threads);
+  threads.resize(max_t);
+  for (uint32_t i = 0; i < max_t; i++) {
+    threads.at(i) = std::thread(&ThreadPool::thread_loop, this);
+  }
+}
---
+++ src/thread_pool/thread_pool.h
@@ -0,0 +1,29 @@
+#ifndef MCMV_CORE_SRC_THREAD_POOL_H_
+#define MCMV_CORE_SRC_THREAD_POOL_H_
+
+#include <functional>
+#include <mutex>
+#include <queue>
+
+// modified from https://stackoverflow.com/questions/15752659/thread-pooling-in-c11
+
+class ThreadPool {
+ public:
+    explicit ThreadPool(int max_t);
+  void queue_job(const std::function<void()>& job);
+  void stop();
+  bool busy();
+
+ private:
+  void thread_loop();
+
+  bool should_terminate = false;
+  int max_t;
+  int active_t = 0;
+  std::mutex queue_mutex;
+  std::condition_variable mutex_condition;
+  std::vector<std::thread> threads;
+  std::queue<std::function<void()>> jobs;
+};
+
+#endif //MCMV_CORE_SRC_THREAD_POOL_H_
