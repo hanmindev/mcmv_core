afe49af8e0f1f75b0a68239a10a3c5cc72777ca8
Hanmin Kim <hanminkim123@gmail.com>
2023-06-03 00:48:48 -0700

    feat(armature): add IArmatureImporter interface

--- src/import/armature_importer.h
+++ src/import/armature_importer.h
@@ -8,4 +8,21 @@ struct JointMotion {
   Vector3 offset;
 };
 
+class ArmatureModel {
+ public:
+  int joint_count;
+  Joint *joints;
+
+  ArmatureModel(int joint_count, Joint *joints) : joint_count(joint_count), joints(joints) {}
+  ~ArmatureModel() = default;
+};
+
+class IArmatureImporter {
+ public:
+  virtual ~IArmatureImporter() = default;
+
+  virtual ArmatureModel *get_model() = 0;
+  virtual vector<JointMotion *> get_animation_frames() = 0;
+};
+
 #endif //MCMV_CORE_SRC_IMPORT_ARMATURE_IMPORTER_H_
