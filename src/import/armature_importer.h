e50ea9b909f6b0c9a8f6fa10a757776cf1902ee5
Hanmin Kim <hanminkim123@gmail.com>
2023-06-03 11:39:22 -0700

    feat(armature): add IArmatureImporter interface

--- src/import/armature_importer.h
+++ src/import/armature_importer.h
@@ -8,6 +8,14 @@ struct JointMotion {
   Vector3 offset;
 };
 
+template<typename T>
+class ConcurrentItem {
+ public:
+  ConcurrentItem(T item) : item(item) {}
+  T item;
+  mutex mutex = std::mutex();
+};
+
 class ArmatureModel {
  public:
   int joint_count;
@@ -23,6 +31,7 @@ class IArmatureImporter {
 
   virtual ArmatureModel *get_model() = 0;
   virtual vector<JointMotion *> get_animation_frames() = 0;
+  virtual ConcurrentItem<vector<JointMotion *>> *get_concurrent_animation_frames() = 0;
 };
 
 #endif //MCMV_CORE_SRC_IMPORT_ARMATURE_IMPORTER_H_
