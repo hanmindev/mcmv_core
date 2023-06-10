3ed94b0ca76341feeab6fb118052cc55d74849ab
Hanmin Kim <hanminkim123@gmail.com>
2023-06-09 23:13:19 -0700

    feat(export): add bedrock armature exporter

---
+++ src/export/bedrock/armature_exporter.h
@@ -0,0 +1,20 @@
+#ifndef MCMV_CORE_SRC_OUTPUT_BEDROCK_ARMATURE_EXPORTER_H_
+#define MCMV_CORE_SRC_OUTPUT_BEDROCK_ARMATURE_EXPORTER_H_
+
+using namespace std;
+
+struct ExporterConfig {
+  float fps;
+};
+
+class IArmatureModelExporter {
+ public:
+  virtual void export_armature_model(string path, string name) = 0;
+};
+
+class IArmatureAnimationExporter {
+ public:
+  virtual void export_armature_animation(string path, string name, ExporterConfig config) = 0;
+};
+
+#endif //MCMV_CORE_SRC_OUTPUT_BEDROCK_ARMATURE_EXPORTER_H_
---
+++ src/export/bedrock/bedrock_exporter.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include "bedrock_exporter.h"
+#include "../../import/bb/bb_parser.h"
+#include "../../math/euler.h"
+
+void BedrockExporter::export_armature_animation(string path, string name, ExporterConfig config) {
+  json j;
+
+  j["format_version"] = "1.8.0";
+
+  json bones;
+
+  for (auto &joint : joints) {
+    json bone;
+
+    json rotation;
+    json position;
+
+    for (int i = 0; i < motion_frames.size(); i++) {
+      auto &motion_frame = motion_frames[i];
+      string frame_name = std::to_string(i / config.fps);
+      Euler e = Euler(Order::zyx, motion_frame->rotation);
+      rotation[frame_name] = {e.x, e.y, e.z};
+      position[frame_name] = {motion_frame->offset.x, motion_frame->offset.y, motion_frame->offset.z};
+    }
+
+    bone["rotation"] = rotation;
+    bone["position"] = position;
+
+    bones[joint.name] = bone;
+  }
+  j["animations"]["animation."s + name + "new"s]["description"]["identifier"] = bones;
+
+    ofstream o(path + "\\" + name + ".animation.json");
+    o << j.dump(4);
+    o.close();
+}
+
+BedrockExporter::BedrockExporter(vector<Joint> joints, vector<JointMotion *> motion_frames) {
+  this->joints = std::move(joints);
+  this->motion_frames = std::move(motion_frames);
+}
