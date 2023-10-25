de43ef6c4fb10c8327f6dbd3035594074108681a
DanJHaas <DanHaas@protonmail.com>
2023-10-25 01:05:05 -0400

    add lib directories

--- scripts/populate_libs.sh
+++ scripts/populate_libs.sh
@@ -3,13 +3,12 @@
 @echo off
 cd ../libs
 
-mkdir imgui
+mkdir imgui imguifiledialog glfw
+
 git clone https://github.com/ocornut/imgui.git imgui
 
-mkdir imguifiledialog
 git clone https://github.com/aiekick/ImGuiFileDialog.git imguifiledialog
 
-mkdir glfw
 git clone https://github.com/glfw/glfw.git glfw
 
 echo DONE!
